e27f7e9246b18863edd5e02741d10b5237b394a0
Rodion Zherdev <r.zherdev@vigbo.com>
2025-06-01 11:03:22 +0400

    feat: логически разбивает функции подсчета и вывода

--- main.go
+++ main.go
@@ -6,10 +6,39 @@ import (
 )
 
 func main() {
-	const indexScore float64 = 2
-	userHeight := 1.8
-	userWeight := 100.0
-	BodyWeightIndex := userWeight / math.Pow(userHeight, indexScore)
+	userHeight, userWeight := getUserParameters()
 
-	fmt.Print(BodyWeightIndex)
+	if userHeight <= 0 || userWeight <= 0 {
+		fmt.Println("Ошибка: рост и вес должны быть положительными числами")
+
+		return
+	}
+
+	bodyWeightIndex := calculateBodyWeight(userWeight, userHeight)
+
+	outputResult(bodyWeightIndex)
+}
+
+func getUserParameters() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+
+	fmt.Print("Введите ваш рост в сантиметрах: ")
+	fmt.Scanln(&userHeight)
+	fmt.Print("Введите ваш вес в килограммах: ")
+	fmt.Scanln(&userWeight)
+
+	return userHeight, userWeight
+}
+
+func calculateBodyWeight(userWeight, userHeight float64) float64 {
+	const indexScore float64 = 2
+	bodyWeightIndex := userWeight / math.Pow(userHeight/100, indexScore)
+
+	return bodyWeightIndex
+}
+
+func outputResult(bodyWeightIndex float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", bodyWeightIndex)
+	fmt.Print(result)
 }
