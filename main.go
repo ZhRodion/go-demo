b9e6330e20d9d37b64369f18f62cef7603d3cba1
Rodion Zherdev <r.zherdev@vigbo.com>
2025-06-02 09:38:29 +0400

    feat: Добавляем логику ретрая

--- main.go
+++ main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
 
 func main() {
-	userHeight, userWeight := getUserParameters()
 
-	if userHeight <= 0 || userWeight <= 0 {
-		fmt.Println("Ошибка: рост и вес должны быть положительными числами")
+	defer func() {
+		if recover := recover(); recover != nil {
+			fmt.Println("Recover ", recover)
+		}
+	}()
 
-		return
+	fmt.Println("__ Калькулятор индекса массы тела __")
+
+	for {
+		userHeight, userWeight := getUserParameters()
+
+		bodyWeightIndex, err := calculateBodyWeight(userWeight, userHeight)
+
+		if err != nil {
+			// fmt.Println(err)
+			// continue
+			panic("Рост и вес должны быть положительными числами")
+		}
+
+		outputResult(bodyWeightIndex)
+
+		isRepeatCalculation := repeatCalculation()
+
+		if !isRepeatCalculation {
+			break
+		}
 	}
-
-	bodyWeightIndex := calculateBodyWeight(userWeight, userHeight)
-
-	outputResult(bodyWeightIndex)
 }
 
 func getUserParameters() (float64, float64) {
@@ -31,25 +49,44 @@ func getUserParameters() (float64, float64) {
 	return userHeight, userWeight
 }
 
-func calculateBodyWeight(userWeight, userHeight float64) float64 {
+func calculateBodyWeight(userWeight, userHeight float64) (float64, error) {
 	const indexScore float64 = 2
+
+	if userHeight <= 0 || userWeight <= 0 {
+		return 0, errors.New("рост и вес должны быть положительными числами")
+	}
+
 	bodyWeightIndex := userWeight / math.Pow(userHeight/100, indexScore)
 
-	return bodyWeightIndex
+	return bodyWeightIndex, nil
 }
 
 func outputResult(bodyWeightIndex float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", bodyWeightIndex)
 
-	if bodyWeightIndex < 18.5 {
+	switch {
+	case bodyWeightIndex < 18.5:
 		result += " - недостаточный вес"
-	} else if bodyWeightIndex >= 18.5 && bodyWeightIndex <= 24.9 {
+	case bodyWeightIndex >= 18.5 && bodyWeightIndex <= 24.9:
 		result += " - нормальный вес"
-	} else if bodyWeightIndex >= 25 && bodyWeightIndex <= 29.9 {
+	case bodyWeightIndex >= 25 && bodyWeightIndex <= 29.9:
 		result += " - избыточный вес"
-	} else if bodyWeightIndex >= 30 {
+	case bodyWeightIndex >= 30:
 		result += " - ожирение"
 	}
 
 	fmt.Println(result)
+}
+
+func repeatCalculation() bool {
+	var userChoice string
+
+	fmt.Print("Хотите повторить расчет? (y/n): ")
+	fmt.Scanln(&userChoice)
+
+	if userChoice == "y" || userChoice == "Y" {
+		return true
+	} else {
+		return false
+	}
 }
